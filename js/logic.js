a4b21b4e98e9b14ffe20b7364c5067b77d0d1c1d
refactoringg <renflenpersonal@gmail.com>
2023-08-17 16:30:11 +0530

    Coolio

---
+++ js/logic.js
@@ -0,0 +1,6 @@
+function download() {
+    const dialog = document.querySelector('.dialog')
+    console.log('lol')
+    dialog.style.visibility = 'visible';
+    dialog.style.animation = 'dialogopen 0.5s cubic-bezier(0.165, 0.84, 0.44, 1)';
+}
